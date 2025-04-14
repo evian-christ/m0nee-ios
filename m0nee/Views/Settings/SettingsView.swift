ab70a090a3eeb418da6d36057fd16f20c3f2978a
evian-christ <58662680+evian-christ@users.noreply.github.com>
2025-04-14 17:50:41 +0100

    Changed app name to Expenses

--- m0nee/Views/Settings/SettingsView.swift
+++ m0nee/Views/Settings/SettingsView.swift
@@ -10,31 +10,31 @@ struct SettingsView: View {
 					Label("General", systemImage: "gearshape")
 						.frame(minHeight: 44)
 				}
-				
+
 				NavigationLink(destination: AppearanceSettingsView()) {
 					Label("Appearance", systemImage: "paintbrush")
 						.frame(minHeight: 44)
 				}
-				
+
 				NavigationLink(destination: BudgetSettingsView()) {
 					Label("Budget", systemImage: "chart.pie.fill")
 						.frame(minHeight: 44)
 				}
-				
+
 				NavigationLink(destination: CategorySettingsView(store: store)) {
 					Label("Categories", systemImage: "folder")
 						.frame(minHeight: 44)
 				}
-				
+
 				NavigationLink(destination: StorageSettingsView(store: store)) {
 					Label("Storage", systemImage: "externaldrive")
 						.frame(minHeight: 44)
 				}
 			}
-			
+
 			Section {
 				VStack(alignment: .center) {
-					Text("Monir v1.0.0")
+					Text("Expenses v1.0.0")
 						.font(.footnote)
 						.foregroundColor(.gray)
 					Text("Made with ❤️ in SwiftUI")
@@ -42,6 +42,8 @@ struct SettingsView: View {
 						.foregroundColor(.secondary)
 				}
 				.frame(maxWidth: .infinity)
+				.listRowBackground(Color.clear)
+				.listRowSeparator(.hidden)
 			}
 		}
 		.navigationTitle("Settings")
