85d862ef8394e8ef29b4add532ab5c435a893fc8
evian-christ <58662680+evian-christ@users.noreply.github.com>
2025-07-15 17:15:28 +0100

    feat: added korean translation for decimal setting

--- m0nee/Models/DecimalDisplayMode.swift
+++ m0nee/Models/DecimalDisplayMode.swift
@@ -6,4 +6,15 @@ enum DecimalDisplayMode: String, CaseIterable, Identifiable, Codable {
 	case none = "None"
 	
 	var id: String { self.rawValue }
+	
+	var localizedTitle: String {
+		switch self {
+		case .automatic:
+			return NSLocalizedString("DecimalDisplayMode.Automatic", comment: "Automatic decimal display mode")
+		case .twoPlaces:
+			return NSLocalizedString("DecimalDisplayMode.TwoPlaces", comment: "Two decimal places")
+		case .none:
+			return NSLocalizedString("DecimalDisplayMode.None", comment: "No decimals")
+		}
+	}
 }
